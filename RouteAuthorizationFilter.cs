a56f9c8064f83b407401768fce94764308c531fc
Leonardo Aguiar <leo_aguiar10@hotmail.com>
2021-02-12 16:36:17 -0300

    Allow one route without authorization

--- RouteAuthorizationFilter.cs
+++ RouteAuthorizationFilter.cs
@@ -14,38 +14,41 @@ namespace routing
         {
             JsonResult jsonResult;
             base.OnActionExecuting(context);
-            // if(context.HttpContext.User.Identity.IsAuthenticated)
-            var isAuthenticated = true;
-            if(isAuthenticated)
+            var actionName = context.RouteData.Values["action"] as string;
+            if(actionName != "AllowedFunctionalities") 
             {
-                var _db = context.HttpContext.RequestServices.GetService(typeof(DbConetxt)) as DbConetxt;
-                var actionName = context.RouteData.Values["action"] as string;
-                
-                var usuarioAssociadoAFuncionalidade =( 
-                    from rf in _db.RotasFuncionalidades
-                    join f in _db.Funcionalidades on rf.IdFuncionalidade equals f.Id
-                    join urf in _db.UsuariosRotasFuncionalidades on rf.Id equals urf.IdRotaFuncionalidade into joinedUrf
-                    from jurf in joinedUrf.DefaultIfEmpty() 
-                    where 
-                    f.ActionName == actionName
-                    && jurf != null
-                    && jurf.IdUsuario == 1
-                    select f
-                ).FirstOrDefault();
-
-                var autorizado = usuarioAssociadoAFuncionalidade != null;
-                if(!autorizado)
+                // if(context.HttpContext.User.Identity.IsAuthenticated)
+                var isAuthenticated = true;
+                if(isAuthenticated)
                 {
-                    // jsonResult = new JsonResult(new { mensagem = "Usuário não tem acesso a funcionalidade" });
-                    jsonResult = new JsonResult(new UnauthorizedAccessException("Usuário não tem acesso a funcionalidade"));
+                    var _db = context.HttpContext.RequestServices.GetService(typeof(DbConetxt)) as DbConetxt;
+                    
+                    var usuarioAssociadoAFuncionalidade =( 
+                        from rf in _db.RotasFuncionalidades
+                        join f in _db.Funcionalidades on rf.IdFuncionalidade equals f.Id
+                        join urf in _db.UsuariosRotasFuncionalidades on rf.Id equals urf.IdRotaFuncionalidade into joinedUrf
+                        from jurf in joinedUrf.DefaultIfEmpty() 
+                        where 
+                        f.ActionName == actionName
+                        && jurf != null
+                        && jurf.IdUsuario == 1
+                        select f
+                    ).FirstOrDefault();
+
+                    var autorizado = usuarioAssociadoAFuncionalidade != null;
+                    if(!autorizado)
+                    {
+                        // jsonResult = new JsonResult(new { mensagem = "Usuário não tem acesso a funcionalidade" });
+                        jsonResult = new JsonResult(new UnauthorizedAccessException("Usuário não tem acesso a funcionalidade"));
+                        context.Result = jsonResult;
+                    }
+                }
+                else
+                {
+                    jsonResult = new JsonResult(new AuthenticationException("Usuário não autenticado"));
+                    // jsonResult = new JsonResult(new { mensagem = "Usuário não autenticado" });
                     context.Result = jsonResult;
                 }
-            }
-            else
-            {
-                jsonResult = new JsonResult(new AuthenticationException("Usuário não autenticado"));
-                // jsonResult = new JsonResult(new { mensagem = "Usuário não autenticado" });
-                context.Result = jsonResult;
             }
         }
     }
